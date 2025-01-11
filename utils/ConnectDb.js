06c8e32b691bcad5a16237a8d35638b4025f7d6a
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-11 11:55:05 +0330

    Customer model created.

--- utils/ConnectDb.js
+++ utils/ConnectDb.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const ConnectDb = () => {
   if (mongoose.connection[0].readyState) return;
   mongoose.connect(process.env.MONGO_URI);
+  console.log("connected to DB");
 };
 
 export default ConnectDb;
